711a198eacfac3a05403955168d116c7272d5aca
Trabelsi Chaima <trabelsi10chaima@gmail.com>
2024-02-04 01:27:03 +0100

    almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       colors: {
